6bebd274c939a0241347fc222c11db333b2e7822
Anil Charles <anilcharles@Anils-MacBook.domain.name>
2019-06-15 22:01:27 +0530

    mac support

--- yt_channel.py
+++ yt_channel.py
@@ -10,91 +10,147 @@ import youtube_dl
 class yt_downloader():
 
     def __init__(self):
-        # yt channel url from command line 
+        '''
+        1. extract channel link from command line
+        2. finds name of the channel
+        3. create channel name folder to store playlist name folder
+        '''
+        
+        # check if url is provided on command line
         try:
+            
+            # yt channel url from command line
             url = sys.argv[1]
-            print(url)
-            self.url = url + '/playlists'            
-            soup = self.grabber(url + '/about') 
-            author_name = soup.find(id='channel-title').text
-            # current directory
-            self.path = os.getcwd() + '\\'+  author_name
             
         except:
+            
+            # exit if not channel is not provided
             print('\nplease provide a youtube channel\n')
             sys.exit()
+        
+        # to redirect yt channel to playlist
+        self.url = url + '/playlists'
+        
+        # check if driver is loaded and ready to grab html source code
+        try:
+        
+            # redirect to about page
+            soup = self.grabber(url + '/about')
+            # finds name of the channel
+            author_name = soup.find(id='channel-title').text
+            # inside channel name directory
+            self.path = os.path.join(os.getcwd(),author_name)
             
+        except:
+            
+                sys.exit()
+
         self.playlist()
-       
         self.playlist_details()
         
         
-    # grabs source code of the page
+    
     def grabber(self,url=None):
-
+        '''
+        1. check if url is provided
+        2. download html source code
+        '''
+        
         # if no url is provided, uses the default one from the command line
         if not url:
+            
             # default url from the command line
             url = self.url
+        
         # checking if correct url is provided
         if url:
+            
             # code from selenium for downloading dynamic page's source code
             options = webdriver.ChromeOptions()
             options.add_argument('--ignore-certificate-errors')
             options.add_argument('--incognito')
+            # these prevents the browser from launching
             options.add_argument('--headless')
             
             # chromedriver should be present in the working directory
             try:
+                if os.name == 'nt':
                 
-                driver = webdriver.Chrome(os.getcwd() + '\chromedriver.exe' , chrome_options=options)
+                    # initializing webdriver for chrome
+                    
+                    driver = webdriver.Chrome(os.getcwd() + '\chromedriver.exe' , chrome_options=options)
                 
-               
+                        
+                else:
+                
+                    driver = webdriver.Chrome(os.getcwd() + '/chromedriver' , chrome_options=options)
+                
+
+                # to get the particular website
                 driver.get(url)
                 # download page source code
                 page = driver.execute_script('return document.body.innerHTML')
                 # converting to beautiful soup obj for manipulation
                 soup = BeautifulSoup(''.join(page), 'html.parser')
+            
                 
             except:
+                
+                # missing chromedriver.exe for windows and chromedriver.sh for mac
                 print('\nchromedriver not found\n')
+                # exit from the program
                 sys.exit()
+            
+                
 
         
             
     
         else:
-        
+            
             print('\nplease provide a youtube channel\n')
+
         
+                    
         return soup
 
-    # gets all playlists and their corresponding video urls
+
+    
+                    
+
+    
     def playlist(self):
+        '''
+        1. checks if provided url is www.youtube.com
+        2. extracts all the playlists
+        '''
+        
         # takes default command line url
         url = self.url
         
         if url:
+            
             # checking if url is from youtube
             if 'www.youtube.com' not in url:
                 print('\nplease provide a youtube channel\n')
                 sys.exit()
-                      
-            
             
             # grabs the source code of that current url
             soup = self.grabber(url)
-            
-            
             # extracting playlist urls from the source code
             x = soup.find_all('a',class_="yt-simple-endpoint style-scope yt-formatted-string")
         # check for playlist
-        if not x:   
+        if not x:
+            
+            # if the channel does not contain any playlist
             print("no playlists") 
             sys.exit()
+        
         else:
+            
             # store the playlist in a list called self.playlists
             self.playlists = []
+            
             # loops around each parts of the source code
             for value in x:               
                 # playlist urls present in href class
@@ -107,6 +163,11 @@ class yt_downloader():
             
 
     def playlist_details(self):
+        '''
+        1. find video urls for each playlist
+        2. create playlist name folder
+        3. filter video urls
+        '''
         
         # each youtube links in a playlist
         for each_playlist in self.playlists:
@@ -115,13 +176,13 @@ class yt_downloader():
             # grabs the source code of each playlist
             soup = self.grabber(url=each_playlist)
             # extracts playlist name from source code
-            playlist_name = [content.contents[0].strip() for content in soup.find_all('a',class_='yt-simple-endpoint style-scope yt-formatted-string')][0]  
-            
-            
+            playlist_name = [content.contents[0].strip() for content in soup.find_all('a',class_='yt-simple-endpoint style-scope yt-formatted-string')][0]
             # check if folder with playlist name exist
-            if not os.path.exists(self.path + '\\' + playlist_name):
+            self.loc = os.path.join(self.path,playlist_name)
+            if not os.path.exists(self.loc):
                 # create new folder with playlist name           
-                os.makedirs(self.path + '\\' + playlist_name)
+                os.makedirs(self.loc)
+            
             # playlist containing urls
             playlist_video_urls = [value.get('href') for value in soup.find_all('a',class_='yt-simple-endpoint style-scope ytd-playlist-video-renderer')]
             '''
@@ -131,6 +192,7 @@ class yt_downloader():
             filtered: /watch?v=ZF8d6kMDoLk
             '''
             playlist_video_urls_filtered =  [a.split('&list')[0] for a in playlist_video_urls]
+            
             # going through each yt video urls of a single playlist
             for video_url in playlist_video_urls_filtered:
                 # calling download_video function with each url as parameter
@@ -139,9 +201,14 @@ class yt_downloader():
 
 
     def download_video(self,url,playlist_name):
+        '''
+        1. download video using youtube-dl library
+        2. stores downloaded videos in corresponding playlist name folder
+        3. skips video that are private
+        '''
        
         #options for youtube-dl
-        ydl_opts = {'outtmpl': self.path + '\\' + playlist_name + '\%(title)s.%(ext)s'}
+        ydl_opts = {'outtmpl': os.path.join(self.loc,'\%(title)s.%(ext)s')}
         # formatting youtube.com/watch?v=something      
         url = 'https://www.youtube.com' + url
         
@@ -149,7 +216,13 @@ class yt_downloader():
         
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([url])
+    
+        except KeyboardInterrupt:
+            
+            sys.exit()
+        
         except:
+            
             print('\n'+ playlist_name + '\nvideo unavailable skipping\n')
         
        
